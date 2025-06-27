29ef2f77726bf945466d0a51b5b09b7b31c5aee5
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-06-27 15:08:39 +0200

    .

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Pressable, Text } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { html as h } from "react-strict-dom";
 
 export default function Index() {
@@ -8,18 +8,38 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        margin: 20,
+        gap: 20,
       }}
     >
       <h.button>Pre focus</h.button>
       <Text>RN Text</Text>
-      <Text accessible role="button" onPress={() => {}}>
-        RN Text - with onPress
-      </Text>
+      <Text onPress={() => {}}>RN Text - with onPress</Text>
       <Pressable onPress={() => {}}>
         <Text accessible onPress={() => {}}>
           RN Text in Pressable
         </Text>
       </Pressable>
+
+      <View accessible>
+        <Text>RN Text in an accessibleView</Text>
+      </View>
+
+      <Text>
+        This is a longer RN Text with a{" "}
+        <Pressable onPress={() => {}}>
+          <Text>RN Text in Pressable</Text>
+        </Pressable>{" "}
+        inside it.
+      </Text>
+      <Text>
+        This is a longer RN Text with a{" "}
+        <View accessible>
+          <Text>RN Text in an accessible View</Text>
+        </View>
+        inside it.
+      </Text>
+
       <h.span>RSD Span</h.span>
       <h.h1>RSD H1</h.h1>
       <h.button>Post focus</h.button>
